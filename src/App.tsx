26bf4ae7f3a6993e554bd0d2d024768d7189b3ed
Nate Geslin <teamtomkins23@gmail.com>
2023-11-15 18:46:02 -0600

    Initialize!

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <div className="flex flex-col align-center">
+      <div className={"w-full"}>
+        <div>GAME SETTINGS</div>
+        <div>QUESTIONS</div>
+      </div>
+      <div className="mt-3">MAP</div>
+    </div>
+  );
+}
